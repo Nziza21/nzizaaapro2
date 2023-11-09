396521b00461390ae9640a6faf3a37c7e19b1119
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-11-09 23:45:43 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const App = () => {
   const [catData, setCatData] = useState([]);
 
   useEffect(() => {
-    // Fetch cat images from the API
     fetch('https://api.thecatapi.com/v1/images/search?format=json&limit=10')
       .then((response) => response.json())
       .then((data) => setCatData(data))
