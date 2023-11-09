c05231e990691f729d26eb9e85e8cacd5bd1f969
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-11-09 23:10:21 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './style.css'; // Import the CSS file
 
 ReactDOM.render(
   <React.StrictMode>
